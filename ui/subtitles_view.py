1a89f41234119dbacf9c810900a750ac6cba5d2b
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-08-20 13:39:31 +0700

    Create minimum working subtitles_view.py

---
+++ ui/subtitles_view.py
@@ -0,0 +1,101 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QHBoxLayout, QFrame, QWidget, QVBoxLayout, QBoxLayout
+
+
+def create_frame(style_sheet: str) -> QFrame:
+    frame = QFrame()
+    frame.setAttribute(Qt.WA_TranslucentBackground)
+    frame.setWindowFlags(Qt.X11BypassWindowManagerHint)
+    frame.setStyleSheet(style_sheet)
+    return frame
+
+
+def create_vertical_linear_layout(parent: QWidget, spacing: int) -> QVBoxLayout:
+    vertical_linear_layout = QVBoxLayout(parent)
+    vertical_linear_layout.setSpacing(spacing)
+    vertical_linear_layout.setContentsMargins(0, 0, 0, 0)
+    return vertical_linear_layout
+
+
+def create_horizontal_linear_layout() -> QHBoxLayout:
+    horizontal_linear_layout = QHBoxLayout()
+    horizontal_linear_layout.setContentsMargins(0, 0, 0, 0)
+    horizontal_linear_layout.setSpacing(20)
+    horizontal_linear_layout.addStretch()
+    return horizontal_linear_layout
+
+
+def split_string_by_words_limit(text: str, limit: int) -> tuple:
+    result = text, ""
+    words_count_to_split = 4
+    words_list = text.split()
+
+    if len(words_list) > limit:
+        first_line = " ".join(words_list[0:words_count_to_split])
+        second_line = " ".join(words_list[words_count_to_split:len(words_list)])
+        result = first_line, second_line
+
+    return result
+
+
+class SubtitlesLine:
+    def __init__(self, parent_layout: QBoxLayout):
+        self.line_layout = create_horizontal_linear_layout()
+        parent_layout.addLayout(self.line_layout)
+
+    def set_text(self, text: str):
+        self.clear_text()
+        for word in text.split():
+            label = QLabel(word)
+            self.line_layout.addWidget(label)
+
+    def clear_text(self):
+        self.line_layout.parentWidget().hide()
+
+        while not self.line_layout.isEmpty():
+            item = self.line_layout.takeAt(0)
+            if not item:
+                return
+
+            widget = item.widget()
+            if widget:
+                widget.deleteLater()
+
+
+class SubtitlesView:
+    def __init__(self, config):
+        self.cfg = config
+        self.frame = create_frame(self.cfg.style_subs)
+        self.outer_layout = create_vertical_linear_layout(self.frame, self.cfg.subs_padding_between_lines)
+
+        self.first_subs_line = SubtitlesLine(self.outer_layout)
+        self.second_subs_line = SubtitlesLine(self.outer_layout)
+
+    def submit_subs(self, subs_text: str):
+        subs_lines = split_string_by_words_limit(subs_text, 5)
+
+        self.first_subs_line.set_text(subs_lines[0])
+        self.second_subs_line.set_text(subs_lines[1])
+
+        self.set_subs_frames_x_y_axis()
+        self.show()
+
+    def show(self):
+        self.frame.show()
+
+    def hide(self):
+        self.frame.hide()
+
+    def set_subs_frames_x_y_axis(self):
+        self.frame.adjustSize()
+
+        w = self.frame.geometry().width()
+        h = self.frame.height = self.frame.geometry().height()
+        x = (self.cfg.screen_width / 2) - (w / 2)
+
+        if self.cfg.subs_top_placement_B:
+            y = self.cfg.subs_screen_edge_padding
+        else:
+            y = self.cfg.screen_height - self.cfg.subs_screen_edge_padding - h
+
+        self.frame.setGeometry(int(x), int(y), 0, 0)
