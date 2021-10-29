b2bfe8bc7c3e177effb73f8ab59a2e1041ae9ba2
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-10-29 13:24:07 +0700

    Reformat the main script to comply with PEP 8

--- interSubs.py
+++ interSubs.py
@@ -12,29 +12,29 @@ from ui.subtitles_view import SubtitlesView
 
 
 def main():
-	print("[python] Starting interSubs ...")
+    print("[python] Starting interSubs ...")
 
-	mpv_socket_path = sys.argv[1]
-	mpv = Mpv(mpv_socket_path)
-	subs_file_path = sys.argv[2]
+    mpv_socket_path = sys.argv[1]
+    mpv = Mpv(mpv_socket_path)
+    subs_file_path = sys.argv[2]
 
-	app = QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
-	cfg = config
-	cfg.screen_width = app.primaryScreen().size().width()
-	cfg.screen_height = app.primaryScreen().size().height()
+    cfg = config
+    cfg.screen_width = app.primaryScreen().size().width()
+    cfg.screen_height = app.primaryScreen().size().height()
 
-	subs_view = SubtitlesView(cfg)
-	popup_view = PopupView(cfg)
-	subs_view.register_text_hover_event_handler(popup_view.pop)
+    subs_view = SubtitlesView(cfg)
+    popup_view = PopupView(cfg)
+    subs_view.register_text_hover_event_handler(popup_view.pop)
 
-	subs_data_source_worker = SubtitlesDataSourceWorker(subs_file_path)
-	subs_data_source_worker.on_subtitles_changed.connect(popup_view.hide)
-	subs_data_source_worker.on_subtitles_changed.connect(subs_view.submit_subs)
-	subs_data_source_worker.start()
+    subs_data_source_worker = SubtitlesDataSourceWorker(subs_file_path)
+    subs_data_source_worker.on_subtitles_changed.connect(popup_view.hide)
+    subs_data_source_worker.on_subtitles_changed.connect(subs_view.submit_subs)
+    subs_data_source_worker.start()
 
-	app.exec_()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
-	main()
+    main()
