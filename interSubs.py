ff7a94e6c0e892fb87aba758ab1d1fc43f144533
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-10-29 13:14:55 +0700

    Move thread_translations into lookup_data_source

--- interSubs.py
+++ interSubs.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python
 
 import sys
-import threading
-import time
 
-from PyQt5.QtCore import Qt, QObject, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication
 
 import config
@@ -12,45 +9,6 @@ from data_provider.subtitles_data_source import SubtitlesDataSourceWorker
 from mpv import Mpv
 from ui.popup_view import PopupView
 from ui.subtitles_view import SubtitlesView
-
-
-class thread_translations(QObject):
-	get_translations = pyqtSignal(str, int, bool)
-
-	@pyqtSlot()
-	def main(self):
-		while 1:
-			to_new_word = False
-
-			try:
-				word, globalX = config.queue_to_translate.get(False)
-			except:
-				time.sleep(config.update_time)
-				continue
-
-			# changing cursor to hourglass during translation
-			QApplication.setOverrideCursor(Qt.WaitCursor)
-
-			threads = []
-			for translation_function_name in config.translation_function_names:
-				threads.append(threading.Thread(target = globals()[translation_function_name], args = (word,)))
-			for x in threads:
-				x.start()
-			while any(thread.is_alive() for thread in threads):
-				if config.queue_to_translate.qsize():
-					to_new_word = True
-					break
-				time.sleep(config.update_time)
-
-			QApplication.restoreOverrideCursor()
-
-			if to_new_word:
-				continue
-
-			if config.block_popup:
-				continue
-
-			self.get_translations.emit(word, globalX, False)
 
 
 def main():
