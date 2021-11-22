00780d07d87ef5cb2fd44e056266f0497429249c
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-11-22 19:07:44 +0700

    Extract caching in its own module
    Separate logic of storing and retrieving cached data from retrieving
    data from remote source.

--- data_provider/dict_cc.py
+++ data_provider/dict_cc.py
@@ -1,4 +1,3 @@
-import os
 import re
 from typing import Tuple, List
 from urllib.parse import quote
@@ -9,19 +8,12 @@ from bs4 import BeautifulSoup
 import config
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36"
-CACHE_DIR_PATH = "urls/"
 URL = "https://%s-%s.dict.cc/?s=%s"
 
 
 def translate(word: str) -> Tuple[List[List[str]], List[str]]:
     compiled_url = URL % (config.lang_from, config.lang_to, quote(word))
-    file_path = CACHE_DIR_PATH + compiled_url.replace('/', "-")
-
-    if os.path.isfile(file_path):
-        pairs_list = get_from_local(file_path)
-    else:
-        pairs_list = get_from_remote(compiled_url)
-        store_in_local(file_path, pairs_list)
+    pairs_list = get_from_remote(compiled_url)
 
     return pairs_list, ['', '']
 
@@ -49,21 +41,5 @@ def _find_all_original_translation_pairs(content: str) -> List[List[str]]:
         pairs_list.append([original, translation])
         if config.number_of_translations_to_save and len(pairs_list) > config.number_of_translations_to_save:
             break
-
-    return pairs_list
-
-
-def store_in_local(file_path: str, pairs_list: List[List[str]]) -> None:
-    print('\n\n'.join(e[0] + '\n' + e[1] for e in pairs_list), file=open(file_path, 'a'))
-    print('\n' + '=====/////-----' + '\n', file=open(file_path, 'a'))
-
-
-def get_from_local(file_path: str) -> List[List[str]]:
-    pairs_list = []
-    lines = open(file_path).read().split('=====/////-----')
-    if len(lines[0].strip()):
-        for pair in lines[0].strip().split('\n\n'):
-            pair = pair.split('\n')
-            pairs_list.append([pair[0], pair[1]])
 
     return pairs_list
