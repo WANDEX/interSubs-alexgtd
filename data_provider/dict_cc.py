56532e9d1dc91aa37555178d996fef9486719823
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-11-22 19:17:57 +0700

    Change returning type to list of pairs

--- data_provider/dict_cc.py
+++ data_provider/dict_cc.py
@@ -11,14 +11,14 @@ USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 URL = "https://%s-%s.dict.cc/?s=%s"
 
 
-def translate(word: str) -> Tuple[List[List[str]], List[str]]:
+def translate(word: str) -> Tuple[List[Tuple[str, str]], List[str]]:
     compiled_url = URL % (config.lang_from, config.lang_to, quote(word))
     pairs_list = get_from_remote(compiled_url)
 
     return pairs_list, ['', '']
 
 
-def get_from_remote(url: str) -> List[List[str]]:
+def get_from_remote(url: str) -> List[Tuple[str, str]]:
     response = requests.get(url, headers={'User-Agent': USER_AGENT})
     if response.status_code != requests.codes.ok:
         response.raise_for_status()
@@ -26,7 +26,7 @@ def get_from_remote(url: str) -> List[List[str]]:
     return _find_all_original_translation_pairs(response.text)
 
 
-def _find_all_original_translation_pairs(content: str) -> List[List[str]]:
+def _find_all_original_translation_pairs(content: str) -> List[Tuple[str, str]]:
     content = re.sub('<div style="float:right;color:#999">\d*</div>', '', content)
     content = re.sub('<span style="color:#666;font-size:10px;padding:0 2px;position:relative;top:-3px">\d*</span>', '',
                      content)
@@ -38,7 +38,7 @@ def _find_all_original_translation_pairs(content: str) -> List[List[str]]:
         pairs = tr.find_all('td', class_='td7nl')
         original = pairs[1].get_text()
         translation = pairs[0].get_text()
-        pairs_list.append([original, translation])
+        pairs_list.append((original, translation))
         if config.number_of_translations_to_save and len(pairs_list) > config.number_of_translations_to_save:
             break
 
