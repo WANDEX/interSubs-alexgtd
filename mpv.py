afff6a933604a98308e33455b08dbf5f559899bd
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-11-24 16:59:37 +0700

    Refactor mpv module
    * Get rid of duplication and manual string building.
    * Add type hints.

--- mpv.py
+++ mpv.py
@@ -1,36 +1,58 @@
 import os
 import subprocess
-from json import loads
+from json import loads, dumps
+from typing import Union
+
+
+class MpvJsonIpcReplyError(Exception):
+    pass
+
+
+def _build_json_ipc(command_name: str, *values: Union[str, int, bool]) -> str:
+    # resulting JSON IPC should look like this: { "command": ["command_name", "param1", "param2", ...] }
+    cmd_list = [command_name]
+    for v in values:
+        cmd_list.append(v)
+    return dumps({"command": cmd_list})
+
+
+def _parse_json_ipc_reply(output: str) -> Union[str, int, bool]:
+    reply = loads(output)
+    if reply['error'] != "success":
+        raise MpvJsonIpcReplyError(reply['error'])
+    return reply['data']
 
 
 class Mpv:
-    def __init__(self, mpv_socket_path: str):
-        self.socket_path = mpv_socket_path
+    def __init__(self, mpv_socket_file: str):
+        self._socket_file = mpv_socket_file
 
-    def pause(self):
-        os.system(
-            'echo \'{ "command": ["set_property", "pause", true] }\' | socat - "' + self.socket_path + '" > /dev/null')
+    def pause(self) -> None:
+        self._set_property("pause", True)
 
-    def resume(self):
-        os.system(
-            'echo \'{ "command": ["set_property", "pause", false] }\' | socat - "' + self.socket_path + '" > /dev/null')
+    def resume(self) -> None:
+        self._set_property("pause", False)
 
-    def is_paused(self):
-        stdout = subprocess.getoutput(
-            'echo \'{ "command": ["get_property", "pause"] }\' | socat - "' + self.socket_path + '"')
-        try:
-            return loads(stdout)['data']
-        except:
-            return self.is_paused()
+    def is_paused(self) -> bool:
+        return _parse_json_ipc_reply(self._get_property("pause"))
 
-    def is_in_fullscreen(self):
-        stdout = subprocess.getoutput(
-            'echo \'{ "command": ["get_property", "fullscreen"] }\' | socat - "' + self.socket_path + '"')
-        try:
-            return loads(stdout)['data']
-        except:
-            return self.is_in_fullscreen()
+    def is_in_fullscreen(self) -> bool:
+        return _parse_json_ipc_reply(self._get_property("fullscreen"))
 
-    def show_text(self, message, timeout=3000):
-        os.system('echo \'{ "command": ["show-text", "' + message + '", "' + str(
-            timeout) + '"] }\' | socat - "' + self.socket_path + '" > /dev/null')
+    def show_message(self, text: str, timeout: int = 3000) -> None:
+        json_ipc = _build_json_ipc("show-text", text, timeout)
+        os.system(self._construct_shell_command_with_silenced_stdout(json_ipc))
+
+    def _set_property(self, property_name: str, value: bool) -> None:
+        json_ipc = _build_json_ipc("set_property", property_name, value)
+        os.system(self._construct_shell_command_with_silenced_stdout(json_ipc))
+
+    def _get_property(self, property_name: str) -> str:
+        json_ipc = _build_json_ipc("get_property", property_name)
+        return subprocess.getoutput(self._construct_shell_command(json_ipc))
+
+    def _construct_shell_command_with_silenced_stdout(self, json_ipc: str) -> str:
+        return f"{self._construct_shell_command(json_ipc)} > /dev/null"
+
+    def _construct_shell_command(self, json_ipc: str) -> str:
+        return f'echo \'{json_ipc}\' | socat - "{self._socket_file}"'
