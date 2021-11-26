70b27b8a043a5d4eede2346b14ef483525454e92
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-11-26 16:25:01 +0700

    Refactor main lua script

--- main.lua
+++ main.lua
@@ -1,130 +1,147 @@
-local PLUGIN_TOGGLE_KEYBIND = "F5"
-local VISIBILITY_TOGGLE_KEYBIND = "F6"
+local mp = require("mp")
+local msg = require("mp.msg")
+local utils = require("mp.utils")
+
 local IS_AUTO_START_ON = false
+local ADDON_TOGGLE_KEYBIND = "F5"
+local ADDON_VISIBILITY_TOGGLE_KEYBIND = "F6"
 -- for Mac change python3 to python or pythonw
-local PYTHON_COMMAND = 'python3 "%s" "%s" "%s"'
+local PYTHON_COMMAND_NAME = "python3"
 local PYTHON_SCRIPT_FILENAME = "interSubs.py"
 
-local python_script_file_path = mp.get_script_directory().."/"..PYTHON_SCRIPT_FILENAME
-local random_number = math.random(11111111, 99999999)
 
 local function put_cmd_in_bg(cmd) return cmd.." &" end
 
 local function is_there_no_selected_subs()
 	return mp.get_property("sub") == "no" or mp.get_property("sub") == "auto"
-end	
-
-local function create_mpv_socket_file()
-	-- recomend to have it in tmpfs
-	mpv_socket_file_path = "/tmp/mpv_socket"..'_'..random_number
-	mp.set_property("input-ipc-server", mpv_socket_file_path)
 end
 
-local function destroy_mpv_socket_file()
+local python = {}
+
+function python:start(ipc_file, subs_file)
+	self.cmd = PYTHON_COMMAND_NAME..' "%s" "%s" "%s"'
+	self.script_file_path = utils.join_path(mp.get_script_directory(), PYTHON_SCRIPT_FILENAME)
+
+	os.execute(put_cmd_in_bg(self.cmd:format(self.script_file_path, ipc_file, subs_file)))
+end
+
+function python:stop()
+	os.execute(put_cmd_in_bg("pkill -f "..self.script_file_path))
+end
+
+local ipc = {}
+
+function ipc:init()
+	self.file_path = os.tmpname()
+	mp.set_property("input-ipc-server", self.file_path)
+end
+
+function ipc:destroy()
 	mp.set_property("input-ipc-server", "")
-	os.execute(put_cmd_in_bg("rm "..mpv_socket_file_path))
+	os.remove(self.file_path)
 end
 
-local function write_sub_to_file(name, value)
-	if type(value) == "string" then
-		file = io.open(subs_file_path, "w")
-		file:write(value)
-		file:close()
+local subs = {}
+
+function subs:init()
+	self:save_current_settings()
+	self:hide_native_subs()
+
+	self.file_path = os.tmpname()
+	self.on_subs_change = function(_, text)
+		if not text then return end
+		local f = io.open(self.file_path, "w+")
+		f:write(text)
+		f:flush()
+		f:close()
 	end
+	mp.observe_property("sub-text", "string", self.on_subs_change)
 end
 
-local function create_subs_file()
-	-- recomend to have it in tmpfs
-	subs_file_path = "/tmp/mpv_sub"..'_'..random_number
-	mp.observe_property("sub-text", "string", write_sub_to_file)
+function subs:destroy()
+	mp.unobserve_property(self.on_subs_change)
+	os.remove(self.file_path)
+	self:restore_settings()
 end
 
-local function destroy_subs_file()
-	mp.unobserve_property(write_sub_to_file)
-	os.execute(put_cmd_in_bg("rm "..subs_file_path))
+function subs:save_current_settings()
+	self.visibility = mp.get_property("sub-visibility")
+	self.color = mp.get_property("sub-color", "1/1/1/1")
+	self.border_color = mp.get_property("sub-border-color", "0/0/0/1")
+	self.shadow_color = mp.get_property("sub-shadow-color", "0/0/0/1")
 end
 
-local function save_current_subs_settings()
-	sub_visibility = mp.get_property("sub-visibility")
-	sub_color = mp.get_property("sub-color", "1/1/1/1")
-	sub_border_color = mp.get_property("sub-border-color", "0/0/0/1")
-	sub_shadow_color = mp.get_property("sub-shadow-color", "0/0/0/1")
+function subs:restore_settings()
+	mp.set_property("sub-visibility", self.visibility)
+	mp.set_property("sub-color", self.color)
+	mp.set_property("sub-border-color", self.border_color)
+	mp.set_property("sub-shadow-color", self.shadow_color)
 end
 
-local function restore_subs_settings()
-	mp.set_property("sub-visibility", sub_visibility)
-	mp.set_property("sub-color", sub_color)
-	mp.set_property("sub-border-color", sub_border_color)
-	-- mp.set_property("sub-shadow-color", sub_shadow_color)
-end
+function subs:hide_native_subs()
+	mp.set_property("sub-visibility", "yes")
+	mp.set_property("sub-ass-override", "force")
 
-local function hide_native_subs()
 	mp.set_property("sub-color", "0/0/0/0")
 	mp.set_property("sub-border-color", "0/0/0/0")
 	mp.set_property("sub-shadow-color", "0/0/0/0")
 end
 
-local function stop_intersub()
-	mp.command('show-text "Quitting interSubs..."')
+local addon = {
+	is_running = false,
+	is_visible = true
+}
 
-	os.execute(put_cmd_in_bg("pkill -f "..mpv_socket_file_path))
-	destroy_mpv_socket_file()
-	destroy_subs_file()
-	restore_subs_settings()
+function addon:stop()
+	mp.osd_message("Quitting interSubs.")
 
-	mp.unregister_event(stop_intersub)
-	is_running = false
+	python:stop()
+	ipc:destroy()
+	subs:destroy()
+
+	mp.unregister_event(addon.stop)
+	self.is_running = false
 end
 
-local function start_intersub()
+function addon:start()
 	if is_there_no_selected_subs() then
-		mp.command('show-text "Select subtitles before starting interSubs."')
-		return 
+		mp.osd_message("Select subtitles before starting interSubs.")
+		return
 	end
-	mp.command('show-text "Starting interSubs..."')
+	mp.osd_message("Starting interSubs.")
 
-	save_current_subs_settings()
-	mp.set_property("sub-visibility", "yes")
-	-- TODO: check if it needs to be like this
-	mp.set_property("sub-ass-override", "force")
-	hide_native_subs()
+	subs:init()
+	ipc:init()
+	python:start(ipc.file_path, subs.file_path)
 
-	create_subs_file()
-	create_mpv_socket_file()
-
-	os.execute(put_cmd_in_bg(PYTHON_COMMAND:format(python_script_file_path, mpv_socket_file_path, subs_file_path)))
-	mp.register_event("shutdown", stop_intersub)
-	is_running = true
+	mp.register_event("shutdown", addon.stop)
+	self.is_running = true
 end
 
-local function toggle_subs_visibility()
-	if not is_running then return end
-	if is_visible then
-		os.execute(put_cmd_in_bg('touch "'..mpv_socket_file_path..'_hide"'))
-		mp.osd_message("Hiding interSubs.", .8)
-		is_visible = false
+local function toggle_addon_visibility()
+	if not addon.is_running then return end
+	msg.error("NotImplementedError")
+end
+
+local function toggle_addon_running_state()
+	if addon.is_running then
+		addon:stop()
 	else
-		os.execute(put_cmd_in_bg('rm "'..mpv_socket_file_path..'_hide"'))
-		mp.osd_message("Showing interSubs.", .8)
-		is_visible = true
+		addon:start()
 	end
 end
 
-local function run()
-	if is_running then
-		stop_intersub()
-	else
-		start_intersub()
+local function main()
+	mp.add_forced_key_binding(ADDON_TOGGLE_KEYBIND, "start-stop-interSubs", toggle_addon_running_state)
+	mp.add_forced_key_binding(ADDON_VISIBILITY_TOGGLE_KEYBIND, "hide-show-interSubs", toggle_addon_visibility)
+
+	local auto_start = function()
+		if is_there_no_selected_subs() then return end
+		if IS_AUTO_START_ON and not addon.is_running then
+			toggle_addon_running_state()
+		end
 	end
+	mp.register_event("file-loaded", auto_start)
 end
 
-local function on_file_loaded()
-	if is_there_no_selected_subs() then return end
-	if IS_AUTO_START_ON then
-		run()
-	end
-end
-
-mp.add_forced_key_binding(PLUGIN_TOGGLE_KEYBIND, "start-stop-interSubs", run)
-mp.add_forced_key_binding(VISIBILITY_TOGGLE_KEYBIND, "hide-show-interSubs", toggle_subs_visibility)
-mp.register_event("file-loaded", on_file_loaded)
+main()
