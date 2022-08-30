bf1054ba78727f50e7562980dfc9bc00fb6f94b3
alexgtd <49579984+alexgtd@users.noreply.github.com>
2022-08-30 16:51:14 +0700

    Refactor mpv module
    Use socket library to access mpv's `json_ipc` protocol instead of
    shell commands.

--- mpv.py
+++ mpv.py
@@ -1,31 +1,46 @@
-import os
-import subprocess
-from json import loads, dumps
-from typing import Union
+import json
+import random
+import socket
+from collections.abc import Sequence
+from typing import Union, Final
+
+JsonTypes = Union[str, int, float, bool, None]
+
+UTF_8: Final[str] = "utf-8"
+BIN_EOL: Final[bytes] = b'\n'
 
 
 class MpvJsonIpcReplyError(Exception):
     pass
 
 
-def _build_json_ipc(command_name: str, *values: Union[str, int, bool]) -> str:
-    # resulting JSON IPC should look like this: { "command": ["command_name", "param1", "param2", ...] }
-    cmd_list = [command_name]
-    for v in values:
-        cmd_list.append(v)
-    return dumps({"command": cmd_list})
+def build_json_ipc_command(command_name: str, *command_args: JsonTypes, request_id: int = None) -> str:
+    # JSON message form - "{"command":["command_name","param1","param2",...],"request_id":"number"}\n"
+    json_message = {"command": [command_name, *command_args]}
+    if request_id:
+        json_message["request_id"] = request_id
+    return f"{json.dumps(json_message, separators=(',', ':'))}\n"
 
 
-def _parse_json_ipc_reply(output: str) -> Union[str, int, bool]:
-    reply = loads(output)
-    if reply['error'] != "success":
-        raise MpvJsonIpcReplyError(reply['error'])
-    return reply['data']
+def parse_json_ipc_reply(bin_messages: Sequence[bytes], request_id: int) -> JsonTypes:
+    bin_strings = list(filter(None, bin_messages))
+    strings = list(map(lambda b: b.decode(UTF_8), bin_strings))
+    cmd_replies = list(filter(lambda s: "request_id" in s, strings))
+
+    for reply in cmd_replies:
+        message: dict = json.loads(reply)
+        if message["request_id"] == request_id:
+            if message["error"] != "success":
+                raise MpvJsonIpcReplyError(message["error"])
+            return message["data"]
 
 
 class Mpv:
-    def __init__(self, mpv_socket_file: str):
-        self._socket_file = mpv_socket_file
+    def __init__(self, ipc_file_path: str) -> None:
+        self._ipc_file_path = ipc_file_path
+        self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._socket.connect(self._ipc_file_path)
+        self._socket.settimeout(0.01)
 
     def pause(self) -> None:
         self._set_property("pause", True)
@@ -34,25 +49,47 @@ class Mpv:
         self._set_property("pause", False)
 
     def is_paused(self) -> bool:
-        return _parse_json_ipc_reply(self._get_property("pause"))
+        return self._get_property("pause")
 
     def is_in_fullscreen(self) -> bool:
-        return _parse_json_ipc_reply(self._get_property("fullscreen"))
+        return self._get_property("fullscreen")
 
     def show_message(self, text: str, timeout: int = 3000) -> None:
-        json_ipc = _build_json_ipc("show-text", text, timeout)
-        os.system(self._construct_shell_command_with_silenced_stdout(json_ipc))
+        cmd = build_json_ipc_command("show-text", text, timeout)
+        self._send_command(cmd)
 
     def _set_property(self, property_name: str, value: bool) -> None:
-        json_ipc = _build_json_ipc("set_property", property_name, value)
-        os.system(self._construct_shell_command_with_silenced_stdout(json_ipc))
+        cmd = build_json_ipc_command("set_property", property_name, value)
+        self._send_command(cmd)
 
-    def _get_property(self, property_name: str) -> str:
-        json_ipc = _build_json_ipc("get_property", property_name)
-        return subprocess.getoutput(self._construct_shell_command(json_ipc))
+    def _get_property(self, property_name: str) -> JsonTypes:
+        id_ = random.randint(-2 ** 63, 2 ** 63 - 1)
+        cmd = build_json_ipc_command("get_property", property_name, request_id=id_)
+        self._send_command(cmd)
+        return self._get_command_response(id_)
 
-    def _construct_shell_command_with_silenced_stdout(self, json_ipc: str) -> str:
-        return f"{self._construct_shell_command(json_ipc)} > /dev/null"
+    def _send_command(self, json_ipc: str) -> None:
+        self._socket.sendall(bytes(json_ipc, encoding=UTF_8))
 
-    def _construct_shell_command(self, json_ipc: str) -> str:
-        return f'echo \'{json_ipc}\' | socat - "{self._socket_file}"'
+    def _get_command_response(self, request_id: int) -> JsonTypes:
+        return parse_json_ipc_reply(self._get_raw_data_from_socket(), request_id)
+
+    def _get_raw_data_from_socket(self) -> list[bytes]:
+        buffer = b''
+        raw_messages = []
+        try:
+            while True:
+                chunk = self._socket.recv(1024)
+                if not chunk:
+                    break
+
+                buffer += chunk
+                if BIN_EOL in buffer:
+                    for line in buffer.split(BIN_EOL):
+                        raw_messages.append(line)
+                    buffer = b''
+        except TimeoutError:
+            # Use TimeOut exception as an end of a socket reply.
+            pass
+
+        return raw_messages
